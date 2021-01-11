1651f38f5e47cd7b5f0dd5ddbbbb901f6569dc25
Mizraelko <vkoporov@gmail.com>
2021-01-11 20:08:29 +0300

    add error, refactor code

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,13 @@
-import React from "react";
-import { render } from "react-dom";
-
-import "./index.less";
-import App from "./component/App";
-import { store } from "./reducers/index";
-import { Provider } from "react-redux";
+import './index.less'
+import React from 'react'
+import {render} from 'react-dom'
+import App from "./components/App";
+import {Provider} from "react-redux";
+import {store} from "./reducers";
 
 render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById("root")
-);
+    <Provider store={store}>
+        <App/>
+    </Provider>,
+    document.getElementById("root")
+)
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,15 +1,12 @@
-import { combineReducers } from "redux"; // хранит все редьюсеры с приложения
-import { createStore, applyMiddleware } from "redux";
-import reposReducer from "./reposReducers"; // создание стора и подключает thunk
-import { composeWithDevTools } from "redux-devtools-extension";
+import {combineReducers} from "redux";
+import {createStore, applyMiddleware} from "redux";
+import reposReducer from "./reposReducer";
+import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from "redux-thunk";
 
 const rootReducer = combineReducers({
-  //корневой редюссер
-  reposReducer,
-});
+    repos: reposReducer,
 
-export const store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(thunk))
-);
+})
+
+export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
